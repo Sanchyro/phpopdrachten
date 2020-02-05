45f499da0d30e791413efaff7522a8f43ecbe93a
Sanchyro <sanchyro@live.nl>
2020-02-05 14:15:18 +0100

    Alle Hoofdstukken added in de Aside

--- index.php
+++ index.php
@@ -20,10 +20,62 @@
             <h2>Menu</h2>
             <ul>
                 <li>
-                    <a href="hoofdstuk2/opdracht2.1.php">Opdracht 2.1</a>
+                    Hoofdstuk 2
+                    <ul>
+                        <li>
+                            <a href="hoofdstuk2/opdracht2.1.php">Opdracht 2.1</a>
+                        </li>
+                        <li>
+                            <a href="hoofdstuk2/opdracht2.2.php">Opdracht 2.2</a>
+                        </li>
+                    </ul>
                 </li>
                 <li>
-                    <a href="hoofdstuk2/opdracht2.2.php">Opdracht 2.2</a>
+                    Hoofdstuk 3
+                    <ul>
+                        <li>
+                            <a href="hoofdstuk3/opdracht3.1.php">Opdracht 3.1</a>
+                        </li>
+                        <li>
+                            <a href="hoofdstuk3/opdracht3.2.php">Opdracht 3.2</a>
+                        </li>
+                        <li>
+                            <a href="hoofdstuk3/opdracht3.3.php">Opdracht 3.3</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    Hoofdstuk 4
+                    <ul>
+                        <li>
+                            <a href="hoofdstuk4/opdracht4.1.php">Opdracht 4.1</a>
+                        </li>
+                        <li>
+                            <a href="hoofdstuk4/opdracht4.2.php">Opdracht 4.2</a>
+                        </li>
+                        <li>
+                            <a href="hoofdstuk4/opdracht4.3.php">Opdracht 4.3</a>
+                        </li>
+                        <li>
+                            <a href="hoofdstuk4/opdracht4.4.php">Opdracht 4.4</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>Hoofdstuk 5
+                    <ul>
+                        <li>
+                            <a href="hoofdstuk5/opdracht5.1.php">Opdracht 5.1</a>
+                        </li>
+                        <li>
+                            <a href="hoofdstuk5/opdracht5.2.php">Opdracht 5.2</a>
+                        </li>
+                        <li>
+                            <a href="hoofdstuk5/opdracht5.3.php">Opdracht 5.3</a>
+                        </li>
+                        <li>
+                            <a href="hoofdstuk5/opdracht5.4.php">Opdracht 5.4</a>
+                        </li>
+                    </ul>
                 </li>
             </ul>
         </aside>
