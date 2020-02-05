a8de6b57f15a76ca1910e65e259886beeb078ef1
Sanchyro <sanchyro@live.nl>
2020-02-05 14:01:56 +0100

    Eerste Upload, Frame + Opdracht 2.1 gemaakt

---
+++ hoofdstuk2/opdracht2.1.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * User: Sanchyro Eijkemans
+ * Date: 05-02-2020
+ * Time: 13:45
+ * File: opdracht2.1.php
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>PHP Opdrachten</title>
+        <link href="../css/style.css" type="text/css" rel="stylesheet">
+    </head>
+    <body>
+        <header>
+            <h1><?php echo "Uitwerking van PHP-opdrachten"; ?></h1>
+        </header>
+    <aside>
+        <h2>Menu</h2>
+        <ul>
+            <li>
+                <a href="hoofdstuk2/opdracht2.1.php">Opdracht 2.1</a>
+            </li>
+            <li>
+                <a href="hoofdstuk2/opdracht2.2.php">Opdracht 2.2</a>
+            </li>
+        </ul>
+    </aside>
+    <main id="wrapper">
+        <h2>Uitwerkingen</h2>
+        <p><?php echo "Hello World!"; ?></p>
+    </main>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * User: Sanchyro Eijkemans
+ * Date: 05-02-2020
+ * Time: 13:30
+ * File: index.php
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>PHP Opdrachten</title>
+        <link href="css/style.css" type="text/css" rel="stylesheet">
+    </head>
+    <body>
+        <header>
+            <h1><?php echo "Uitwerking van PHP-opdrachten"; ?></h1>
+        </header>
+        <aside>
+            <h2>Menu</h2>
+            <ul>
+                <li>
+                    <a href="hoofdstuk2/opdracht2.1.php">Opdracht 2.1</a>
+                </li>
+                <li>
+                    <a href="hoofdstuk2/opdracht2.2.php">Opdracht 2.2</a>
+                </li>
+            </ul>
+        </aside>
+        <main id="wrapper">
+            <h2>Uitwerkingen</h2>
+        </main>
+    </body>
+</html>
