39bf4894b5142397980b1c08e150d47ffd4cae81
Sanchyro <sanchyro@live.nl>
2020-02-10 14:55:57 +0100

    template.php added

---
+++ template.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * User: Sanchyro Eijkemans
+ * Date: 05-02-2020
+ * Time: 13:45
+ * File: opdracht2.1.php
+ */
+?>
+
+<!-- Include the sidebar and header -->
+<?php
+include("../index.php");
+?>
+
+<!-- The code -->
+<?php
+    /* The code here */
+?>
